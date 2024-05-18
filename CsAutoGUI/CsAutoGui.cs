61e07b7d60d1d2e52017e9d93b7cc53e9eac2954
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-05-18 22:21:45 +0330

    Initial commit

---
+++ CsAutoGUI/CsAutoGui.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CsAutoGUI;
+using System.Drawing;
+public class CsAutoGui
+{
+    
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out Point lpPoint);
+    public Point position()
+    {
+        GetCursorPos(out Point point);
+        return point;
+    }
+}
---
+++ CsAutoGUI/MainTest.cs
@@ -0,0 +1,10 @@
+namespace CsAutoGUI;
+
+public class MainTest
+{
+    static void Main(string[] args)
+    {
+        CsAutoGui cs = new CsAutoGui();
+        Console.WriteLine(cs.position().X);
+    }
+}
