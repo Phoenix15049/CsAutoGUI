a126088f5308be55fc5dee53150ab9ebb1135cd3
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2024-07-06 14:50:07 +0330

    Minor Updates .

--- CsAutoGUI/obj/Debug/net8.0/CsAutoGUI.AssemblyInfo.cs
+++ CsAutoGUI/obj/Debug/net8.0/CsAutoGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsAutoGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb53c50c44d3d26a9585cdf9cdb2be09fd203dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3e00d38fd21befcb50522bc4c34066b9e0089b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsAutoGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsAutoGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
